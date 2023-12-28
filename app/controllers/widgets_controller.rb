72e5aa476f53d4c48ab764bc867e721cf58e1bfd
Andrei Balbo <andreibalbo@gmail.com>
2023-12-28 10:30:36 -0300

    Implement a vanity (custom) url

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,5 +3,12 @@ class WidgetsController < ApplicationController
     @widget = OpenStruct.new(id: params[:id],
                             manufacturer_id: rand(100),
                             name: "Widget #{params[:id]}")
-  end 
+  end
+
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1, name: "Stembolt"),
+      OpenStruct.new(id: 2, name: "Flux Capacitor"),
+    ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :widgets
+  resources :widgets, only: [ :show, :index ]
 
   get "manufacturer/:id", to: "manufacturers#show"
+
+
+  ####
+  # Custom (vanity) routes start here 
+  #
+  # For each new custom route: 
+  #
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard
+
+  # Used in podcast ads for the 'amazing' campaign
+  get "/amazing", to: redirect("/widgets")
+  # get "/amazing", to: "widgets#index" # without redirecting
+
 end
