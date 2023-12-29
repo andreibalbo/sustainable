f02899e5f62812621dcf982b2c0faaa795ce74f2
Andrei Balbo <andreibalbo@gmail.com>
2023-12-29 10:43:35 -0300

    Show nested resources but only one instance variable

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,7 +1,22 @@
 class WidgetsController < ApplicationController 
   def show
+    manufacturer = OpenStruct.new(
+      id: rand(100),
+      name: "Sector 7G",
+      address: OpenStruct.new(
+              id: rand(100),
+              country: "UK"
+      )
+    )
+
+    # Expose only one instance variable to the view.
+    # if we also choose to expose @manufacturer to avoid too much nesting
+    # the developers would have to go back to the controller to see what's
+    # loaded into the manufacturer instance. 
+    # By Exposing a single widget everything is connected and simpler.
     @widget = OpenStruct.new(id: params[:id],
-                            manufacturer_id: rand(100),
+                            manufacturer_id: manufacturer.id,
+                            manufacturer: manufacturer,
                             name: "Widget #{params[:id]}")
   end
 
